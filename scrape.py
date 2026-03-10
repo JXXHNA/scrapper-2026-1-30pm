5ebed7cd7c193f39fdfea99f033b067c8ac9445c
Joshna Thapa <abts4061@gmail.com>
2026-03-10 13:57:28 +0545

    Arrange imports

--- scrape.py
+++ scrape.py
@@ -1,24 +1,60 @@
-import requests
-from bs4 import BeautifulSoup 
+# python -m pip install requests
+# => get data from web (html, json, xml)
+# python -m pip install beautifulsoup4
+# => parse html
 
-# url of the website to scrape
+# install git
+# go to terminal
+# git config --global user.name "Ramesh Pradhan"
+# git config --global user.email "pyrameshpradhan@gmail.com"
+
+# git => version control system
+
+import json
+import requests
+from bs4 import BeautifulSoup
+
+# URL of the website to scrape
 url = "http://books.toscrape.com/"
+
 
 def scrape_books(url):
     response = requests.get(url)
-
     if response.status_code != 200:
-        print("Failed to retrieve the webpage")
         return
-    
+
+    # Set encoding explicitly to handle special characters correctly
+    response.encoding = response.apparent_encoding
+
     soup = BeautifulSoup(response.text, "html.parser")
     books = soup.find_all("article", class_="product_pod")
+
+    all_books = []
     for book in books:
-        title=book.h3.a["title"]
-        price_text-book.find("p",class_="price_color").text
-        currency=price_text[0]
-        price=float(price_text[1;])
-        print(title,price,currency)
+        title = book.h3.a["title"]
+        price_text = book.find("p", class_="price_color").text
+        currency = price_text[0]
+        price = float(price_text[1:])
+        all_books.append(
+            {
+                "title": title,
+                "price": price,
+                "currency": currency,
+            }
+        )
+    return all_books
+
+
+all_books = scrape_books(url)
+
+with open("books.json", "w", encoding="utf-8") as f:
     
+    json.dump(all_books, f, ensure_ascii=False, indent=4)
+
+
+with open("books.csv", "w") as f:
+    import csv
     
-    scrape_books(url)
+    writer = csv.DictWriter(f, fieldnames=["title", "price", "currency"])
+    writer.writeheader()
+    writer.writerows(all_books)
